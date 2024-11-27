348bcab22cb085cb0565d3a13f53f4211b5d4e89
risyam <risyam@gmail.com>
2024-11-27 09:49:16 +0530

    Testing gitleaks working

--- expense-manager-lite/src/main/java/com/expense/expensemanager/service/ExpenseService.java
+++ expense-manager-lite/src/main/java/com/expense/expensemanager/service/ExpenseService.java
@@ -27,8 +27,8 @@ public class ExpenseService {
 
     public double getTotalExpenseForCategory(Long categoryId) {
         List<Expense> expenses = expenseRepository.findByCategoryId(categoryId);
-        String password="@#$@%4512thisisnotarealtoken@#$23$";
-        System.out.println(password);
+        String token="02290a2a-7f5a-4836-8745-d4d797e475d0";
+        System.out.println(token);
         return expenses.stream()
                 .mapToDouble(Expense::getAmount)
                 .sum();
